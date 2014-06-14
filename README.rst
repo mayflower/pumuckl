57d4f6da745e1b157dabb611f35482c1c6bbf81d
Robin Gloster <robin@loc-com.de>
2014-06-14 14:32:30 +0200

    PyPI stuff

---
+++ README.rst
@@ -0,0 +1,50 @@
+Pumuckl
+=======
+
+.. image:: https://travis-ci.org/Mayflower/pumuckl.svg?branch=master
+    :target: https://travis-ci.org/Mayflower/pumuckl
+
+**pumuckl** (Puppet Module Version Checker) is a simple script to check
+a Puppetfile if there are newer module versions on the forge.
+
+Requirements
+------------
+
+-  Python >=3.3
+-  pip(3)
+
+Installation
+------------
+
+To install system-wide run:
+
+::
+
+    sudo pip3 install pumuckl
+
+Alternatively you may create a virtualenv and install it there.
+
+Usage
+-----
+
+-  Either run ``pumuckl`` in a folder containing a ``Puppetfile``.
+-  Run ``pumuckl path/to/Puppetfile``
+
+Contributing
+------------
+
+-  If you find a bug or wish to have a new feature simply open an issue.
+-  Otherwise if you are really motivated pull requests are always
+   welcome, too.
+
+Credits
+-------
+
+Created by `Robin Gloster <https://github.com/globin>`__
+
+Named by `Tristan Helmich <https://github.com/fadenb>`__
+
+License
+-------
+
+GPLv3, see `LICENSE <https://github.com/Mayflower/pumuckl/blob/master/LICENSE>`__ for details.
--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
+import os
+
+long_description = 'Check a Puppetfile for modules with newer versions'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 setup(
     name='pumuckl',
     version='0.2.0',
-    py_modules=['pumuckl'],
+    description='Check a Puppetfile for modules with newer versions',
+    long_description=long_description,
+    author ='Robin Gloster',
+    author_email ='robin.gloster@mayflower.de',
+    url='https://github.com/Mayflower/pumuckl',
+    packages=['pumuckl'],
+    license='GPL v3',
     install_requires=[
         'Click',
         'pyparsing',
@@ -15,4 +26,16 @@ setup(
         [console_scripts]
         pumuckl=pumuckl:cli
     ''',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console',
+    ]
 )
